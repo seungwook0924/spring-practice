d115f44b1baaa7da9f90ea8036942b2e8bd4cd30
Lee SeungWook <leesw1998@gmail.com>
2024-02-17 00:18:45 +0900

    Spring DB1 - Section2(커넥션풀과 데이터소스 이해)

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,56 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+    
+    @Test
+    void dataSourceDriverManager() throws SQLException{
+        //DriverManagerDataSource - 항상 새로운 커넥션 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+    
+    private void useDataSource(DataSource dataSource) throws SQLException{
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException{
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); //커넥션 풀에서 커넥션 생성 시간 대기
+        /*
+        커넥션 풀에서 커넥션을 생성하는 작업은 애플리케이션 실행 속도에 영향을 주지 않기 위해 별도의 쓰레드에서 작동한다.
+        별도의 쓰레드에서 동작하기 때문에 테스트가 먼저 종료되어 버린다.
+        Thread.sleep 을 통해 대기 시간을 주어야 쓰레드 풀에 커넥션이 생성되는 로그를 확인할 수 있다.
+        */
+    }
+}
---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,60 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV1Test {
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() throws Exception{
+        HikariDataSource dataSource = new HikariDataSource(); //의존성 주입
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        log.info("start");
+
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+
+        //findById
+        Member memberById = repository.findById(member.getMemberId());
+        assertThat(memberById).isNotNull();
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    /*
+    커넥션 풀 사용시 `conn0` 커넥션이 재사용 된 것을 확인할 수 있다.
+    테스트는 순서대로 실행되기 때문에 커넥션을 사용하고 다시 돌려주는 것을 반복한다. 따라서 `conn0` 만 사용된 다.
+    웹 애플리케이션에 동시에 여러 요청이 들어오면 여러 쓰레드에서 커넥션 풀의 커넥션을 다양하게 가져가는 상황 을 확인할 수 있다.
+    */
+}
