0b19b32cb21855e19071baf7996eca20f453bb13
Lee SeungWook <leesw1998@gmail.com>
2024-03-11 03:25:39 +0900

    Spring MVC 2 - Validation(Section 5) V4 - finish

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,29 +1,14 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) //수정시에만 적용
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class) //등록시에만 적용
     private Integer quantity;
 
     public Item() {
