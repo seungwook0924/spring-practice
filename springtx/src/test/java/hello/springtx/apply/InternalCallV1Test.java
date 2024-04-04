c36b1bda62a66590e5bb7518ec74cf37f1213bf4
Lee SeungWook <leesw_0924@icloud.com>
2024-04-05 04:45:57 +0900

    Spring DB Section 09 - 스프링 트랜잭션 이해

---
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,61 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1Config {
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal(); //외부에서 @Transactional 이 붙은 메서드를 실행하면 트랜잭션이 적용되지 않는다. -> 따로 클래스를 만들어야 한다.
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive =
+                    TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
---
+++ springtx/src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -0,0 +1,62 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+    @Autowired
+    BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        //BasicService$$EnhancerBySpringCGLIB...
+        log.info("aop class={}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    @Transactional(readOnly = true)
+    static class BasicService {
+
+        //우선순위는 항상 **더 구체적이고 자세한 것이 높은 우선순위를 가진다.
+        //클래스 레벨에서 (readOnly = true)이지만 메서드 레벨에서 (readOnly = false) 이기때문에 tx()는 읽기 작업과 쓰기 작업 모두 할 수 있다.
+        //(readOnly = true) : 읽기만 가능
+        //(readOnly = false) : 읽기. 쓰기 모두 가능
+        @Transactional(readOnly = false)
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive(); //현재 쓰레드에 트랜잭션이 적용되어 있는지 확인할 수 있는 기능
+            log.info("tx active={}", txActive);
+        }
+        public void nonTx() {
+            log.info("call nonTx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive(); //현재 쓰레드에 트랜잭션이 적용되어 있는지 확인할 수 있는 기능
+            log.info("tx active={}", txActive);
+        }
+    }
+}
