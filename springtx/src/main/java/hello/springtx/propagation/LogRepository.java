79404ccd5f1362405315fdaac7cf9b49c550b2ca
Lee SeungWook <leesw_0924@icloud.com>
2024-04-09 22:59:10 +0900

    Spring DB Section 11 - finish

--- springtx/src/main/java/hello/springtx/propagation/LogRepository.java
+++ springtx/src/main/java/hello/springtx/propagation/LogRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -14,7 +15,7 @@ import java.util.Optional;
 public class LogRepository {
     private final EntityManager em;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void save(Log logMessage) {
         log.info("log 저장");
         em.persist(logMessage);
@@ -25,6 +26,12 @@ public class LogRepository {
     }
 
     public Optional<Log> find(String message) {
+
+        /*
+        getResultList 메소드를 호출하여 쿼리의 실행 결과를 리스트로 가져온다.
+        이후 Java 8의 스트림 API를 사용하여 해당 리스트를 스트림으로 변환하고, findAny 메소드를 통해 결과 리스트 중 임의의 하나를 선택한다.
+        findAny는 Optional<Log> 타입을 반환
+         */
         return em.createQuery("select l from Log l where l.message = :message", Log.class)
                 .setParameter("message", message)
                 .getResultList().stream().findAny();
--- springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,10 +3,12 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
