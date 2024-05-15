788a213c6fc931336d0f1c764c862fdd33f4239e
Lee SeungWook <leesw_0924@icloud.com>
2024-05-16 05:57:39 +0900

    ORM 표준 JPA 기본편 - init

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long ID;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
